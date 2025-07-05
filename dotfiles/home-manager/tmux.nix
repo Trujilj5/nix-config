e41b0bcca57beb861093b2c452b8f66609daef02
John <john.trujillo@crsymmetry.com>
2025-07-05 19:08:36 +0000

    wip

--- dotfiles/home-manager/tmux.nix
+++ dotfiles/home-manager/tmux.nix
@@ -6,38 +6,24 @@
   home.packages = with pkgs; [
     tmux-sessionizer
   ];
+  
+  # Install TPM (Tmux Plugin Manager)
+  home.file.".tmux/plugins/tpm" = {
+    source = pkgs.fetchFromGitHub {
+      owner = "tmux-plugins";
+      repo = "tpm";
+      rev = "v3.1.0";
+      sha256 = "sha256-CeI9Wq6tHqV68woE11lIY4cLotwORWD+bcwZOSFaLxs=";
+    };
+    recursive = true;
+  };
 
   # Enable and configure Tmux
   programs.tmux = {
     enable = true;
     clock24 = true;
     
-    # Enable plugins
-    plugins = with pkgs.tmuxPlugins; [
-      {
-        plugin = catppuccin;
-        extraConfig = ''
-          set -g @catppuccin_flavor "macchiato"
-          set -g @catppuccin_window_status_style "rounded"
-          set -g @catppuccin_status_left_separator "█"
-          set -g @catppuccin_status_right_separator "█"
-          set -g @catppuccin_date_time_text "%Y-%m-%d %H:%M"
-          set -g @catppuccin_window_left_separator ""
-          set -g @catppuccin_window_right_separator " "
-          set -g @catppuccin_window_middle_separator " █"
-          set -g @catppuccin_window_number_position "right"
-          set -g @catppuccin_window_default_fill "number"
-          set -g @catppuccin_window_default_text "#W"
-          set -g @catppuccin_window_current_fill "number"
-          set -g @catppuccin_window_current_text "#W"
-          set -g @catppuccin_status_modules_right "directory session"
-          set -g @catppuccin_status_modules_left ""
-          set -g @catppuccin_status_fill "icon"
-          set -g @catppuccin_status_connect_separator "no"
-          set -g @catppuccin_directory_text "#{pane_current_path}"
-        '';
-      }
-    ];
+    # TPM configuration will be handled in tmux.conf
     
     # Custom configuration from file
     extraConfig = builtins.readFile ../config/tmux/tmux.conf;
