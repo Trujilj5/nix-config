74b31b0aefa9a316a9503e78c9a2fc41c45ddf71
John <john.trujillo@crsymmetry.com>
2025-08-21 16:28:05 -0700

    wip

--- dotfiles/home-manager/lazysql.nix
+++ dotfiles/home-manager/lazysql.nix
@@ -1,54 +1,40 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
-  programs.lazysql = {
-    enable = true;
-    settings = {
-      theme = {
-        activeBorderColor = [
-          "#8aadf4"
-          "bold"
-        ];
-        inactiveBorderColor = [
-          "#a5adcb"
-        ];
-        optionsTextColor = [
-          "#8aadf4"
-        ];
-        selectedLineBgColor = [
-          "#363a4f"
-        ];
-        selectedRowBgColor = [
-          "#363a4f"
-        ];
-        primaryTextColor = [
-          "#cad3f5"
-        ];
-        secondaryTextColor = [
-          "#a5adcb"
-        ];
-        successTextColor = [
-          "#a6da95"
-        ];
-        errorTextColor = [
-          "#ed8796"
-        ];
-        warningTextColor = [
-          "#eed49f"
-        ];
-        searchingActiveBorderColor = [
-          "#eed49f"
-        ];
-        tableHeaderColor = [
-          "#8aadf4"
-        ];
-        keyColor = [
-          "#f4dbd6"
-        ];
-        valueColor = [
-          "#cad3f5"
-        ];
-      };
-    };
-  };
+  home.packages = with pkgs; [
+    lazysql
+  ];
+
+  xdg.configFile."lazysql/config.yml".text = ''
+    theme:
+      activeBorderColor:
+        - "#8aadf4"
+        - "bold"
+      inactiveBorderColor:
+        - "#a5adcb"
+      optionsTextColor:
+        - "#8aadf4"
+      selectedLineBgColor:
+        - "#363a4f"
+      selectedRowBgColor:
+        - "#363a4f"
+      primaryTextColor:
+        - "#cad3f5"
+      secondaryTextColor:
+        - "#a5adcb"
+      successTextColor:
+        - "#a6da95"
+      errorTextColor:
+        - "#ed8796"
+      warningTextColor:
+        - "#eed49f"
+      searchingActiveBorderColor:
+        - "#eed49f"
+      tableHeaderColor:
+        - "#8aadf4"
+      keyColor:
+        - "#f4dbd6"
+      valueColor:
+        - "#cad3f5"
+  '';
 }
