401bb30ec51c81a79428e076c7f98b67e6ee6728
John <john.trujillo@crsymmetry.com>
2025-09-05 20:40:59 -0700

    wip

--- dev-shells/i-console-dev.nix
+++ dev-shells/i-console-dev.nix
@@ -4,6 +4,10 @@
     nodejs_24
     tsx
     typescript
+    gcc
+    gnumake
+    python3
+    pkg-config
   ]);
 in
 {
@@ -15,19 +19,17 @@ in
   fhs = pkgs.buildFHSEnv {
     name = "i-console-dev-fhs";
     targetPkgs = pkgs: (with pkgs; [
+      # Only packages that specifically need FHS
       bun
       nodejs_24
     ]);
     multiPkgs = pkgs: (with pkgs; [
+      # Only system libraries needed for dynamic linking
       glibc
       libgcc
       stdenv.cc.cc.lib
       openssl
       zlib
-      gcc
-      gnumake
-      python3
-      pkg-config
     ]);
     runScript = "zsh";
   };
