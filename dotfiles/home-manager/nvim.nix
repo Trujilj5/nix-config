4ffcb4434555174892ac9cdb1c58bee016448f46
John <john.trujillo@crsymmetry.com>
2025-10-01 12:40:06 -0700

    wip

--- dotfiles/home-manager/nvim.nix
+++ dotfiles/home-manager/nvim.nix
@@ -130,6 +130,26 @@
       '';
   };
 
+  # https://github.com/nvim-treesitter/nvim-treesitter#i-get-query-error-invalid-node-type-at-position
+  xdg.configFile."nvim/parser".source =
+    let
+      parsers = pkgs.symlinkJoin {
+        name = "treesitter-parsers";
+        paths = (pkgs.vimPlugins.nvim-treesitter.withPlugins (plugins: with plugins; [
+          bash
+          c
+          lua
+          markdown
+          nix
+          python
+          rust
+          vim
+          vimdoc
+        ])).dependencies;
+      };
+    in
+    "${parsers}/parser";
+
   # Normal LazyVim config here, see https://github.com/LazyVim/starter/tree/main/lua
   xdg.configFile."nvim".source = ../../dotfiles/config/nvim;
 }
