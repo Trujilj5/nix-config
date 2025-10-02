e5df3c3a076c66dfef8e35bf7e6aef845b408a03
John <john.trujillo@crsymmetry.com>
2025-10-02 13:24:42 -0700

    wip

--- dotfiles/config/nvim/lua/plugins/terminal.lua
+++ dotfiles/config/nvim/lua/plugins/terminal.lua
@@ -1,17 +1,32 @@
--- Simple terminal configuration for LazyVim
+-- Terminal configuration for LazyVim using Snacks
 return {
   {
     "folke/snacks.nvim",
+    opts = function(_, opts)
+      opts = opts or {}
+      opts.terminal = vim.tbl_deep_extend("force", opts.terminal or {}, {
+        win = {
+          height = 0.8,
+          width = 0.8,
+          style = "float",
+          backdrop = 60,
+          wo = {
+            winblend = 10,
+          },
+        },
+      })
+      return opts
+    end,
     keys = {
-      -- Disable default terminal keymaps
+      -- Disable default keymaps
       { "<c-/>", false },
       { "<c-_>", false },
       
-      -- Add <C-t> terminal keymaps
+      -- Custom <C-t> terminal toggle (main terminal)
       {
         "<C-t>",
         function()
-          Snacks.terminal()
+          Snacks.terminal.toggle()
         end,
         desc = "Toggle Terminal",
         mode = "n",
@@ -19,33 +34,54 @@ return {
       {
         "<C-t>",
         function()
-          vim.cmd("close")
+          local win = vim.api.nvim_get_current_win()
+          if vim.w[win].snacks_win then
+            vim.w[win].snacks_win:hide()
+          else
+            vim.cmd("close")
+          end
         end,
-        desc = "Close Terminal",
+        desc = "Hide Terminal", 
         mode = "t",
       },
       
-      -- Multiple terminals with unique IDs
+      -- Multiple terminals using different commands to create unique IDs
       {
         "<leader>t1",
         function()
-          Snacks.terminal(nil, { id = "term1" })
+          Snacks.terminal.toggle("terminal-1")
         end,
         desc = "Terminal 1",
       },
       {
-        "<leader>t2",
+        "<leader>t2", 
         function()
-          Snacks.terminal(nil, { id = "term2" })
+          Snacks.terminal.toggle("terminal-2")
         end,
         desc = "Terminal 2",
       },
       {
         "<leader>t3",
         function()
-          Snacks.terminal(nil, { id = "term3" })
+          Snacks.terminal.toggle("terminal-3")
         end,
         desc = "Terminal 3",
+      },
+      
+      -- Keep LazyVim's ft and fT variants working
+      {
+        "<leader>ft",
+        function()
+          Snacks.terminal.toggle(nil, { cwd = LazyVim.root() })
+        end,
+        desc = "Terminal (Root Dir)",
+      },
+      {
+        "<leader>fT",
+        function()
+          Snacks.terminal.toggle()
+        end,
+        desc = "Terminal (cwd)",
       },
     },
   },
