20fdf14ceea629bfdf3322d4fca4d9a405414721
John <john.trujillo@crsymmetry.com>
2025-10-03 09:52:45 -0700

    wip

--- dotfiles/home-manager/tmux.nix
+++ dotfiles/home-manager/tmux.nix
@@ -16,5 +16,25 @@
     recursive = true;
   };
 
+  home.file.".tmux/plugins/tmux-resurrect" = {
+    source = pkgs.fetchFromGitHub {
+      owner = "tmux-plugins";
+      repo = "tmux-resurrect";
+      rev = "v4.0.0";
+      sha256 = "sha256-5Fh6MJkrbp+H3L8ePpXiflccZ/y2unRnAv7aJK8xS/U=";
+    };
+    recursive = true;
+  };
+
+  home.file.".tmux/plugins/tmux-continuum" = {
+    source = pkgs.fetchFromGitHub {
+      owner = "tmux-plugins";
+      repo = "tmux-continuum";
+      rev = "v3.1.0";
+      sha256 = "sha256-nqa7hGG2V8FU/cWFU9mmCdifTTvgePwLwlCLVsQfHGw=";
+    };
+    recursive = true;
+  };
+
   home.file.".config/tmux/tmux.conf".source = ../config/tmux/tmux.conf;
 }
