c6e3712e602fd1c31187d6e9210728cf8770a46b
John <john.trujillo@crsymmetry.com>
2025-10-03 14:12:37 -0700

    wip

--- dotfiles/config/nvim/lua/plugins/diagnostics.lua
+++ dotfiles/config/nvim/lua/plugins/diagnostics.lua
@@ -1,13 +1,33 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      diagnostics = {
-        underline = true,
+    opts = function(_, opts)
+      opts.diagnostics = opts.diagnostics or {}
+      opts.diagnostics.underline = true
+      opts.diagnostics.update_in_insert = false
+      opts.diagnostics.virtual_text = false
+      opts.diagnostics.severity_sort = true
+
+      -- Configure undercurl highlights
+      vim.diagnostic.config({
+        underline = {
+          severity = { min = vim.diagnostic.severity.HINT },
+        },
+        virtual_text = false,
+        signs = true,
         update_in_insert = false,
-        virtual_text = false, -- Disable text at end of line
         severity_sort = true,
-      },
-    },
+      })
+
+      -- Ensure undercurl is visible
+      vim.cmd([[
+        highlight DiagnosticUnderlineError gui=undercurl guisp=#ed8796
+        highlight DiagnosticUnderlineWarn gui=undercurl guisp=#f5a97f
+        highlight DiagnosticUnderlineInfo gui=undercurl guisp=#8aadf4
+        highlight DiagnosticUnderlineHint gui=undercurl guisp=#8bd5ca
+      ]])
+
+      return opts
+    end,
   },
 }
