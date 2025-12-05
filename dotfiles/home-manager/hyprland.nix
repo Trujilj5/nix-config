52157e7a4e159f747a985893254505394378d3ee
John <john.trujillo@crsymmetry.com>
2025-12-04 18:20:31 -0700

    wip

--- dotfiles/home-manager/hyprland.nix
+++ dotfiles/home-manager/hyprland.nix
@@ -4,6 +4,7 @@
   wayland.windowManager.hyprland = {
     enable = true;
     xwayland.enable = true;
+    portalPackage = null;  # Use portal from system config instead
     extraConfig = builtins.readFile ../config/hypr/hyprland.conf;
   };
 
