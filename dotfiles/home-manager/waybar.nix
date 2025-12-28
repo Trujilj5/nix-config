ca2c1cd641aaee9a65e95dd20bee28c75ece396e
John <john.trujillo@crsymmetry.com>
2025-12-28 12:50:44 -0700

    wip

--- dotfiles/home-manager/waybar.nix
+++ dotfiles/home-manager/waybar.nix
@@ -8,29 +8,11 @@
   home.file = {
     ".config/waybar/config".source = config.lib.file.mkOutOfStoreSymlink "/home/john/nixos/dotfiles/config/waybar/config";
     ".config/waybar/style.css".source = config.lib.file.mkOutOfStoreSymlink "/home/john/nixos/dotfiles/config/waybar/style.css";
-    ".config/waybar/scripts/control-workspace-notifications.sh" = {
-      source = config.lib.file.mkOutOfStoreSymlink "/home/john/nixos/dotfiles/config/waybar/scripts/control-workspace-notifications.sh";
-      executable = true;
-    };
-    ".config/waybar/scripts/english-date.sh" = {
-      source = config.lib.file.mkOutOfStoreSymlink "/home/john/nixos/dotfiles/config/waybar/scripts/english-date.sh";
-      executable = true;
-    };
-    ".config/waybar/scripts/waybar-startup.sh" = {
-      source = config.lib.file.mkOutOfStoreSymlink "/home/john/nixos/dotfiles/config/waybar/scripts/waybar-startup.sh";
-      executable = true;
-    };
-    ".config/waybar/scripts/weather-phoenix.sh" = {
-      source = config.lib.file.mkOutOfStoreSymlink "/home/john/nixos/dotfiles/config/waybar/scripts/weather-phoenix.sh";
-      executable = true;
-    };
-    ".config/waybar/scripts/weather-us.py" = {
-      source = config.lib.file.mkOutOfStoreSymlink "/home/john/nixos/dotfiles/config/waybar/scripts/weather-us.py";
-      executable = true;
-    };
-    ".config/waybar/scripts/workspace-notifications.py" = {
-      source = config.lib.file.mkOutOfStoreSymlink "/home/john/nixos/dotfiles/config/waybar/scripts/workspace-notifications.py";
-      executable = true;
-    };
+    ".config/waybar/scripts/control-workspace-notifications.sh".source = config.lib.file.mkOutOfStoreSymlink "/home/john/nixos/dotfiles/config/waybar/scripts/control-workspace-notifications.sh";
+    ".config/waybar/scripts/english-date.sh".source = config.lib.file.mkOutOfStoreSymlink "/home/john/nixos/dotfiles/config/waybar/scripts/english-date.sh";
+    ".config/waybar/scripts/waybar-startup.sh".source = config.lib.file.mkOutOfStoreSymlink "/home/john/nixos/dotfiles/config/waybar/scripts/waybar-startup.sh";
+    ".config/waybar/scripts/weather-phoenix.sh".source = config.lib.file.mkOutOfStoreSymlink "/home/john/nixos/dotfiles/config/waybar/scripts/weather-phoenix.sh";
+    ".config/waybar/scripts/weather-us.py".source = config.lib.file.mkOutOfStoreSymlink "/home/john/nixos/dotfiles/config/waybar/scripts/weather-us.py";
+    ".config/waybar/scripts/workspace-notifications.py".source = config.lib.file.mkOutOfStoreSymlink "/home/john/nixos/dotfiles/config/waybar/scripts/workspace-notifications.py";
   };
 }
