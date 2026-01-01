3c175acc31cd7fb5f46598d1c9e2aa0f061c2eae
John <john.trujillo@crsymmetry.com>
2025-12-31 23:02:07 -0700

    wip

--- desktop.nix
+++ desktop.nix
@@ -77,7 +77,7 @@
     filezilla
     popsicle
     (writeShellScriptBin "brave" ''
-      exec ${brave}/bin/brave --force-device-scale-factor=1.0 --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer --ozone-platform=wayland "$@"
+      exec ${brave}/bin/brave --force-device-scale-factor=1.0 --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer --ozone-platform=wayland --disable-session-crashed-bubble "$@"
     '')
     firefox
     spotify
