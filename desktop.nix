4a1c5e8ba1119519a79bf9915711501128097604
John <john.trujillo@crsymmetry.com>
2026-01-05 11:05:30 -0700

    wip

--- desktop.nix
+++ desktop.nix
@@ -102,9 +102,9 @@
     python3Packages.requests
     wlogout
     networkmanagerapplet
-    # (writeShellScriptBin "signal-desktop" ''
-    #   exec ${signal-desktop-bin}/bin/signal-desktop --force-device-scale-factor=0.25 --enable-features=UseOzonePlatform --ozone-platform=wayland "$@"
-    # '')
+    (writeShellScriptBin "signal-desktop" ''
+      exec ${signal-desktop-bin}/bin/signal-desktop --force-device-scale-factor=0.25 --enable-features=UseOzonePlatform --ozone-platform=wayland "$@"
+    '')
     (writeShellScriptBin "zed-project-picker" ''
       #!/usr/bin/env bash
 
--- home.nix
+++ home.nix
@@ -147,21 +147,14 @@
       mimeType = [ "application/pdf" "application/rdf+xml" "application/rss+xml" "application/xhtml+xml" "application/xhtml_xml" "application/xml" "image/gif" "image/jpeg" "image/png" "image/webp" "text/html" "text/xml" "x-scheme-handler/http" "x-scheme-handler/https" "x-scheme-handler/ftp" "x-scheme-handler/chrome" "video/webm" "application/x-xpinstall" ];
     };
 
-    # signal-desktop = {
-    #   name = "Signal";
-    #   genericName = "Secure messenger";
-    #   comment = "Private messaging from your desktop";
-    #   exec = "signal-desktop";
-    #   icon = "signal-desktop";
-    #   type = "Application";
-    #   categories = [ "Network" "InstantMessaging" ];
-    #   mimeType = [ "x-scheme-handler/sgnl" "x-scheme-handler/signalcaptcha" ];
-    # };
+    signal-desktop = { name = "Signal"; genericName = "Secure messenger";
+    comment = "Private messaging from your desktop"; exec = "signal-desktop";
+    icon = "signal-desktop"; type = "Application"; categories = [ "Network"
+    "InstantMessaging" ]; mimeType = [ "x-scheme-handler/sgnl"
+    "x-scheme-handler/signalcaptcha" ]; };
 
-    code = {
-      name = "Code";
-      genericName = "Project Picker";
-      comment = "Open project in Zed using Yazi";
+    code = { name = "Code"; genericName = "Project Picker"; comment = "Open
+    project in Zed using Yazi";
       exec = "ghostty -e zed-project-picker";
       icon = "zed";
       type = "Application";
